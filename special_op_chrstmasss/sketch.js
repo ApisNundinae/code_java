6590f92061f877c9201819e7972d314faf0119b2
ApisNundinae <120191373+ApisNundinae@users.noreply.github.com>
2022-12-23 19:38:49 +0100

    yessir

---
+++ special_op_chrstmasss/sketch.js
@@ -0,0 +1,45 @@
+let speedb = 5 ;
+let flocons = [] ;
+
+function setup() {
+createCanvas(windowWidth, windowHeight) ;
+	
+	for (let counter = 0 ; counter < 100 ; counter ++){
+		flocons.push( new floque() ) ;
+	}
+	noLoop() ;
+}
+
+function draw() {
+background(9,14,34) ;
+	for(let counter = 0 ; counter < 100 ; counter ++){
+		flocons[counter].display() ;
+		
+	}
+}
+
+class floque{
+	constructor(){
+
+		this.posX = random(width) ;
+		this.posY = random(height) ;
+		
+		if(this.posY < windowHeight + 20 ) {
+		this.posY = this.posY - windowHeight -20	 ;
+	
+		}
+	
+				
+	}
+	display(){
+		fill(255) ;
+		noStroke() ;
+		ellipse(this.posX,this.posY,20,20) ;
+		this.posY = this.posY + speedb ;
+	}
+}
+function mousePressed() {
+	loop() ;
+	
+	
+}
